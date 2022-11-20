98f41d2ce5f5176311bdd0d93bd60872e12a6ed3
Laila Chokri <lchokri@e1r2p2.1337.ma>
2022-11-20 21:34:56 +0100

    d01

--- d01/ex04/main.cpp
+++ d01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 18:43:54 by lchokri           #+#    #+#             */
-/*   Updated: 2022/11/20 20:33:53 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/11/20 21:25:07 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int main (int ac, char** av)
     std::cout << "Usage: ./SearchAndReplace filename sting1 string2";
     return 0;
   }
+  size_t indx = 0;
   std::string buff;
+  std::string s1 = av[2];
   buff.append(av[1]);
   std::string oFile = buff + ".replace";
   std::ifstream ReadFile(av[1]);
@@ -30,7 +32,9 @@ int main (int ac, char** av)
   
   while (std::getline(ReadFile, buff))
   {
-    
+    indx += buff.find(s1);
+    buff.erase(indx,s1.size());
+    buff.insert(indx, av[3]);
     writeFile << buff <<  std::endl;
   }
 }
