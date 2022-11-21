10cf6a8a63c6cdc59b52853b73711b5c34424097
Laila Chokri <lchokri@e2r9p4.1337.ma>
2022-11-21 18:25:43 +0100

    d01 Ex04 finished ex03 fixed

--- d01/ex04/main.cpp
+++ d01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 18:43:54 by lchokri           #+#    #+#             */
-/*   Updated: 2022/11/21 10:17:03 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/11/21 18:21:08 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,24 @@ int main (int ac, char** av)
   {
     std::cout << "Error! wrong amount of arguments" << std::endl;
     std::cout << "Usage: ./SearchAndReplace filename sting1 string2"<< std::endl;
-    return 0;
+    return 1;
   }
   size_t indx = 0;
   std::string buff;
   std::string s1 = av[2];
   buff.append(av[1]);
-  std::string oFile = buff + ".replace";
   if (!s1.compare(""))
   {
     std::cout << "Error! S1 can't be empty." << std::endl;
-    return 0;
+    return 1;
   }
   std::ifstream ReadFile(av[1]);
+  if (ReadFile.fail())
+  {
+    std::cout << "Error! file doesn't exist or right permissions aren't granted."<< std::endl;
+    return 1;
+  }
+  std::string oFile = buff + ".replace";
   std::ofstream writeFile(oFile);
   while (std::getline(ReadFile, buff))
   {
