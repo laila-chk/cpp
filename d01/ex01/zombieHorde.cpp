70034495cca00ff8920681688f98e8e63c461e4b
Laila Chokri <lchokri@e2r11p5.1337.ma>
2022-11-22 14:46:29 +0100

    fixed 03

--- d01/ex01/zombieHorde.cpp
+++ d01/ex01/zombieHorde.cpp
@@ -17,6 +17,6 @@ Zombie* zombieHorde(int N, std::string name)
   Zombie* zomZom = new Zombie [N];
 
   for (int i = 0; i < N; i++)
-  zomZom[i].setName(name);
+    zomZom[i].setName(name);
   return (zomZom);
 }
