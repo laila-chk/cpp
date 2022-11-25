d678bd3172a862c4bdc9c1f96f312015cc1cc233
laila-chk <lyly.2356@gmail.com>
2022-11-25 13:53:58 +0100

    starting M02-01

---
+++ d02/ex01/Fixed.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 10:23:17 by lchokri           #+#    #+#             */
+/*   Updated: 2022/11/24 11:57:17 by lchokri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int FixedPt::_frac = 8;
+FixedPt::FixedPt (const int val)
+{
+  _val = val;
+  std::cout << "Default constructor called" <<std::endl;
+}
+
+FixedPt::FixedPt (const float val)
+{
+  _val = val;
+  std::cout << "Default constructor called" <<std::endl;
+}
+
+
+FixedPt::~FixedPt ()
+{
+  std::cout << "Destructor called" <<std::endl;
+}
+
+FixedPt::FixedPt(FixedPt& fp) : _val(fp._val)
+{
+  std::cout << "copy constructor called" <<std::endl;
+}
+
+FixedPt FixedPt::operator = (FixedPt& fp)
+{
+  std::cout << "copy assignement operator called" <<std::endl;
+  this->_val = fp._val;
+  return (*this);
+
+}
+
+int FixedPt::getRawBits(void) const
+{
+  return (this->_val);
+}
+
+void FixedPt::setRawBits(int const raw) 
+{
+  this->_val = raw;
+}
+
