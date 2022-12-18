e2a1d880305dab60162a4832e45a764822ae711a
Laila Chokri <lchokri@e2r8p12.1337.ma>
2022-12-18 15:22:53 +0100

    pushing..

--- d08/ex00/easyfind.hpp
+++ d08/ex00/easyfind.hpp
@@ -1,10 +1,19 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+
 template<typename T> 
 int easyfind(const T& cont, int i)
 {
-  //we gotta look for the 1st occurence of i in cont
+  int j = 0;
+
+  while (cont[j])
+  {
+    if (cont[j] == i)
+      return j;
+  }
+  throw std::exception();
 }
 
 #endif
